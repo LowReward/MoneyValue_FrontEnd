5207b4280ab0260a70e8ba404b6ec38657289107
LowReward <hellsingbarkbark@gmail.com>
2023-05-21 22:31:57 +0200

    Views edited and now used to directly get a render in the dashboard

--- src/components/Dashboard/PairsView.vue
+++ src/components/Dashboard/PairsView.vue
@@ -2,33 +2,30 @@
     <div>
       <h2>Pairs</h2>
       <ul>
-        <!-- Boucle d'affichage pour toutes les pairs contenues dans notre réponse -->
         <li v-for="pair in pairs" :key="pair.id">
           {{ pair.currency_from }} - {{ pair.currency_to }} - {{ pair.request_count }}
+          <button @click="editPair(pair)">Modifier</button>
+          <button @click="deletePair(pair)">Supprimer</button>
         </li>
       </ul>
     </div>
   </template>
   
   <script>
-  // Import de notre instance axios créer et comportant l'envoie authorization bearer
   import axiosClient from '../../plugins/axios.js';
   
   export default {
     name: 'PairsView',
     data() {
       return {
-        // Retourne un tableau contenant toutes nos pairs
         pairs: []
       };
     },
     mounted() {
-      // Appel à l'API pour récupérer les pairs depuis le backend
       this.fetchPairs();
     },
     methods: {
       fetchPairs() {
-        // Effectuer une requête HTTP pour récupérer les pairs depuis le backend
         axiosClient.get('http://localhost:8000/api/pairs')
           .then(response => {
             this.pairs = response.data;
@@ -36,6 +33,14 @@
           .catch(error => {
             console.error(error);
           });
+      },
+      editPair(pair) {
+        // Logique pour éditer la paire
+        console.log('Édition de la paire :', pair);
+      },
+      deletePair(pair) {
+        // Logique pour supprimer la paire
+        console.log('Suppression de la paire :', pair);
       }
     }
   };
