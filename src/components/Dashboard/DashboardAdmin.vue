8154769160027a795b183640ee63c515f9306340
LowReward <hellsingbarkbark@gmail.com>
2023-05-21 22:44:14 +0200

    Navbar stylised on the dashboard

--- src/components/Dashboard/DashboardAdmin.vue
+++ src/components/Dashboard/DashboardAdmin.vue
@@ -1,24 +1,27 @@
 <template>
     <div>
-      <h1>Dashboard</h1>
-      <nav>
-        <ul>
-          <li>
-            <!-- Lien pour accéder à la vue des Pairs -->
-            <router-link to="/admin/dashboard/pairs">Pairs</router-link>
-          </li>
-          <li>
-            <!-- Lien pour accéder à la vue des Currencies -->
-            <router-link to="/admin/dashboard/currencies">Currencies</router-link>
-          </li>
-        </ul>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand " href="#">Dashboard MoneyValue</a>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <!-- Lien pour accéder à la vue des Pairs -->
+              <router-link class="nav-link" to="/admin/dashboard/pairs">Pairs</router-link>
+            </li>
+            <li class="nav-item">
+              <!-- Lien pour accéder à la vue des Currencies -->
+              <router-link class="nav-link" to="/admin/dashboard/currencies">Currencies</router-link>
+            </li>
+          </ul>
+        </div>
       </nav>
       <!-- Le contenu correspondant à l'URL de nav sera affiché à cette emplacement
-        donc quand un clique est produit sur la vue correspondante, un rendue sera produit
-    dans notre balise router-view -->
+          donc quand un clique est produit sur la vue correspondante, un rendue sera produit
+          dans notre balise router-view -->
       <router-view></router-view>
     </div>
   </template>
+  
   
   
   <script>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
 createApp(App).use(router,bootstrap).mount('#app')
