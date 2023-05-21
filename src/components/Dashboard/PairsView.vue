3012110cc0d7164f25ad2c21aafaf2377167ca80
LowReward <hellsingbarkbark@gmail.com>
2023-05-21 23:02:27 +0200

    PairsView stylised with boostrap

--- src/components/Dashboard/PairsView.vue
+++ src/components/Dashboard/PairsView.vue
@@ -1,15 +1,31 @@
 <template>
     <div>
-      <h2>Pairs</h2>
-      <ul>
-        <li v-for="pair in pairs" :key="pair.id">
-          {{ pair.currency_from }} - {{ pair.currency_to }} - {{ pair.request_count }}
-          <button @click="editPair(pair)">Modifier</button>
-          <button @click="deletePair(pair)">Supprimer</button>
-        </li>
-      </ul>
+      <div class="d-flex justify-content-between align-items-center mt-5">
+        <h2>Liste des paires:</h2>
+        <button class="btn btn-primary" @click="addPair">Ajouter une paire</button>
+      </div>
+      <table class="table mt-4">
+        <thead>
+          <tr>
+            <th scope="col">Paires</th>
+            <th scope="col">Nombre de requêtes</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="pair in pairs" :key="pair.id">
+            <td>{{ pair.currency_from }} - {{ pair.currency_to }}</td>
+            <td>{{ pair.request_count }}</td>
+            <td>
+              <button class="btn btn-sm btn-primary " @click="editPair(pair)">Modifier</button>
+              <button class="btn btn-sm btn-danger" @click="deletePair(pair)">Supprimer</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </template>
+  
   
   <script>
   import axiosClient from '../../plugins/axios.js';
