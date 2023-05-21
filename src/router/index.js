642e81355fb5bc3396d8751b0e59eae41f17dc52
LowReward <hellsingbarkbark@gmail.com>
2023-05-21 14:39:54 +0200

    RequiresAuth for dashboard route + router condition for authentified user added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage  from '../components/LoginPage.vue'
 import DashboardAdmin  from '../components/DashboardAdmin.vue'
+import store from '../store';
 
 const routes = [
   {
@@ -12,7 +13,8 @@ const routes = [
     path: '/',
     name: 'DashboardAdmin',
     component: DashboardAdmin,
-    meta: {requeresAuth: true}
+    // Besoin d'être authentifié
+    meta: {requiresAuth: true}
   }
 ]
 
@@ -20,5 +22,18 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// Avant d'atteindre n'importe quelle route
+router.beforeEach((to,from,next) => {
+  // Si la prochaine route nécessite d'être authentifié et que le token est absent
+  if (to.meta.requiresAuth && !store.state.user.token){
+    // Redirection à la page login pour s'authentifier
+    next({name: 'Login'});
+  } else{
+    // Sinon le processus continue
+    next();
+  }
+});
+
 
 export default router
