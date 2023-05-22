1353a1e00807c0d0a00091dbd12242082343c1f9
LowReward <hellsingbarkbark@gmail.com>
2023-05-22 16:15:31 +0200

    Placeholders on email/password inputs

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -8,11 +8,11 @@
               <form @submit.prevent="login">
                 <div class="mb-3">
                   <label for="email" class="form-label">Email:</label>
-                  <input type="email" id="email" v-model="email" class="form-control">
+                  <input type="email" id="email" placeholder="Email" v-model="email" class="form-control">
                 </div>
                 <div class="mb-3">
                   <label for="password" class="form-label">Password:</label>
-                  <input type="password" id="password" v-model="password" class="form-control">
+                  <input type="password" id="password" placeholder="Mot de passe" v-model="password" class="form-control">
                 </div>
                 <button type="submit" class="btn btn-primary">Login</button>
               </form>
